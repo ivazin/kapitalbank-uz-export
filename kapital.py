261929a17e2793a22bc416451edfe960bc13e4e1
Ivan <ivazin@yandex.ru>
2023-02-14 04:06:42 +0000

    Update kapital.py

--- kapital.py
+++ kapital.py
@@ -26,7 +26,6 @@ class KapitalAPI:
         "app-version": app_version,
     }
 
-    # is_loaded = False
     cards_ids = []  # здесь будет список пар: (id, тип_карты)
     accounts_ids = []  # здесь будет список id аккаунтов
 
@@ -39,7 +38,6 @@ class KapitalAPI:
         self.expiry = expiry
         self.app_password = app_password
         if not self._load():
-            # print("NOT LOADED")
             self.first_run()
 
     def _gen_device(self, length=32, chars=None):
@@ -52,7 +50,6 @@ class KapitalAPI:
             pickle.dump(
                 {
                     "device_id": self.device_id,
-                    # 'app_name': self.app_name,
                     "token": self.token,
                     "phone": self.phone,
                 },
@@ -64,20 +61,13 @@ class KapitalAPI:
         try:
             with open(self.kapital_config_file, "rb") as handle:
                 data_loaded = pickle.load(handle)
-                # print("Loaded!")
         except Exception as e:
-            # есил нет кэша, скачаем и сохраним его
-            # print(e)
-            # self.is_loaded = False
             return False
         else:
             self.token = data_loaded.get("token")
             self.device_id = data_loaded.get("device_id")
-            # self.app_name = data_loaded.get('app_name')
             self.phone = data_loaded.get("phone")
-            # print("Load:", self.token, self.device_id, self.phone)
             if self.token != "":
-                # self.is_loaded = True
                 return True
             else:
                 return False
@@ -89,7 +79,6 @@ class KapitalAPI:
         headers = {**self.headers_main}
         response = requests.request("POST", endpoint, headers=headers, data=payload)
         result_json = response.json()
-        # print(result_json)
         assert result_json.get("data", {}).get("message", {}) == "Success"
 
     def check_user(self):
@@ -97,25 +86,19 @@ class KapitalAPI:
         payload = f'{{"pan": "{self.pan}", "expiry": "{self.expiry}"}}'
         headers = {**self.headers_main, "device-id": self.device_id}
         response = requests.request("POST", endpoint, headers=headers, data=payload)
-        # print(response.text)
         result_json = response.json()
-        # print(result_json)
         self.phone = result_json.get("data", {}).get("phone", "")
         assert self.phone != ""
-        # print('Записали телефон:', self.phone)
 
     def send_sms(self):
         endpoint = f"{self.base_url_v2}/login"
         payload = f'{{ "pan": "{self.pan}", "expiry": "{self.expiry}", "password": "{self.app_password}", "reserveSms": "false"}}'
         headers = {**self.headers_main, "device-id": self.device_id}
         response = requests.request("POST", endpoint, headers=headers, data=payload)
-        # print(response.text)
         result_json = response.json()
-        # print(result_json)
         error = result_json.get("errorMessage", "")
         assert error == ""
-        # {"data":{"message":"**** 0837"},"errorMessage":"","timestamp":1676245305489}
-
+        
     def input_sms_code(self):
         self.sms_code = input("Введите код из смски: ")
         print("Ввели:", self.sms_code)
@@ -124,9 +107,7 @@ class KapitalAPI:
         endpoint = f"{self.base_url}/registration/verify/{self.sms_code}/{self.phone}"
         headers = {**self.headers_main, "device-id": self.device_id}
         response = requests.request("POST", endpoint, headers=headers, data="{}")
-        # print(response.text)
         result_json = response.json()
-        # print(result_json)
         error = result_json.get("errorMessage", "")
         assert error == ""
         self.fcm_token = result_json.get("data", {}).get("fcm_token", "")
@@ -144,20 +125,6 @@ class KapitalAPI:
         self.get_token()
         self._save()
 
-    # def get_accounts(self):
-    #     endpoint_cards_list = ['uzcard', 'humo', 'visa', 'wallet']
-    #     endpoint_accounts_list = ['account']
-
-    #     headers = {**self.headers_main, 'device-id': self.device_id, 'token': self.token}
-
-    #     res_data = []
-    #     for endpoint in endpoint_cards_list:
-    #         response = requests.request("GET", f'{self.base_url}/{endpoint}', headers=headers, data={})
-    #         result_json = response.json()
-
-    #         print(response.text)
-    #         print(json.dumps(result_json, indent=4))
-    #         res_data.append(result_json.get("data", {}))
 
     def get_cards_df(self):
         df = pd.DataFrame()
@@ -177,16 +144,13 @@ class KapitalAPI:
                 response = requests.request(
                     "GET", f"{self.base_url}/{c}", headers=headers, data={}
                 )
-            # return response.json().get("data", {})
-            # print(f"CARDS!!! {c}")
-            # print(json.dumps(response.json(), indent=4))
+
             new_df = pd.json_normalize(response.json().get("data", {}))
             if not new_df.empty:
                 new_df["card"] = c
                 df = pd.concat([df, new_df])
                 self.cards_ids.extend([(id, c) for id in new_df["id"].tolist()])
-            # print("List:", self.cards_ids)
-        # print(self.cards_ids)
+
         return df
 
     def get_accounts_df(self):
@@ -207,7 +171,7 @@ class KapitalAPI:
                     "GET", f"{self.base_url}/{c}", headers=headers, data={}
                 )
 
-            # return response.json().get("data", {})
+            
             new_df = pd.json_normalize(response.json().get("data", {}))
             if not new_df.empty:
                 df = pd.concat([df, new_df])
@@ -221,15 +185,14 @@ class KapitalAPI:
         for id, card in self.cards_ids:
             if card == "uzcard":
                 endpoint = f"{self.base_url}/{card}/history?cardId={id}&dateFrom={self.from_epoch}&dateTo={self.to_epoch}"
-                # print("!!!", endpoint)
+                
                 headers = {
                     **self.headers_main,
                     "device-id": self.device_id,
                     "token": self.token,
                 }
                 response = requests.request("GET", endpoint, headers=headers, data={})
-                # print(json.dumps(response.json(), indent=4))
-
+            
                 d = response.json().get("data", [])
                 if len(d) != 0 and isinstance(d, dict):
                     new_df = pd.json_normalize(
@@ -251,17 +214,14 @@ class KapitalAPI:
         for id, card in self.cards_ids:
             if card == "visa":
                 endpoint = f"{self.base_url}/{card}/history?cardId={id}&dateFrom={self.from_epoch}&dateTo={self.to_epoch}"
-                # print("!!!", endpoint)
+                
                 headers = {
                     **self.headers_main,
                     "device-id": self.device_id,
                     "token": self.token,
                 }
                 response = requests.request("GET", endpoint, headers=headers, data={})
-                # print(json.dumps(response.json(), indent=4))
-
                 d = response.json().get("data", [])
-                # print(d)
                 if len(d) != 0 and isinstance(d, list):
                     new_df = pd.json_normalize(response.json().get("data", {}))
                     if not new_df.empty:
@@ -269,8 +229,7 @@ class KapitalAPI:
                         df = pd.concat([df, new_df])
         if not df.empty:
             df["transDate_datetime"] = pd.to_datetime(df["transDate"], unit="ms")
-        # df['udate_datetime'] = pd.to_datetime(df['udate'], unit='ms')
-
+            
         return df
 
     def get_humo_history_df(self):
@@ -280,24 +239,21 @@ class KapitalAPI:
         for id, card in self.cards_ids:
             if card == "humo":
                 endpoint = f"{self.base_url}/{card}/history?cardId={id}&dateFrom={self.from_epoch}&dateTo={self.to_epoch}"
-                # print("!!!", endpoint)
+
                 headers = {
                     **self.headers_main,
                     "device-id": self.device_id,
                     "token": self.token,
                 }
                 response = requests.request("GET", endpoint, headers=headers, data={})
-                # print(json.dumps(response.json(), indent=4))
 
                 d = response.json().get("data", [])
-                # print(d)
+
                 if len(d) != 0 and isinstance(d, list):
                     new_df = pd.json_normalize(response.json().get("data", {}))
                     if not new_df.empty:
                         new_df["card_id"] = id
                         df = pd.concat([df, new_df])
-        # if not df.empty:
-        #     df['transDate_datetime'] = pd.to_datetime(df['transDate'], unit='ms')
 
         return df
 
@@ -308,25 +264,21 @@ class KapitalAPI:
         for id, card in self.cards_ids:
             if card == "wallet":
                 endpoint = f"{self.base_url}/{card}/history?id={id}&startDate={self.from_epoch}&endDate={self.to_epoch}"
-                # print("!!!", endpoint)
+
                 headers = {
                     **self.headers_main,
                     "device-id": self.device_id,
                     "token": self.token,
                 }
                 response = requests.request("GET", endpoint, headers=headers, data={})
-                # print(json.dumps(response.json(), indent=4))
 
                 d = response.json().get("data", [])
-                # print(d)
+
                 if len(d) != 0 and isinstance(d, list):
                     new_df = pd.json_normalize(response.json().get("data", {}))
                     if not new_df.empty:
                         new_df["card_id"] = id
                         df = pd.concat([df, new_df])
-        # if not df.empty:
-        #     df['transDate_datetime'] = pd.to_datetime(df['transDate'], unit='ms')
-        # df['udate_datetime'] = pd.to_datetime(df['udate'], unit='ms')
 
         return df
 
@@ -343,7 +295,6 @@ class KapitalAPI:
             }
             response = requests.request("GET", endpoint, headers=headers, data={})
             d = response.json().get("data", [])
-            # print(d)
             if len(d) != 0 and isinstance(d, list):
                 new_df = pd.json_normalize(
                     response.json().get("data", {}).get("data", {})
@@ -364,16 +315,6 @@ class KapitalAPI:
         t4 = self.get_humo_history_df()
         t5 = self.get_accounts_history_df()
 
-        # df_c.to_excel('export_cards_data.xlsx', index=False)
-        # df_ac.to_excel('export_accounts_data.xlsx', index=False)
-        # t1.to_excel('export_get_uzcard_history_df.xlsx', index=False)
-        # t2.to_excel('export_get_visa_history_df.xlsx', index=False)
-        # t3.to_excel('export_get_wallet_history_df.xlsx', index=False)
-        # t4.to_excel('export_get_humo_history_df.xlsx', index=False)
-        # t5.to_excel('export_get_accounts_history_df.xlsx', index=False)
-        # tmp = pd.concat([t1, t2])
-        # tmp.to_excel('export_all_uzcard+visa_transactions.xlsx', index=False)
-
         with pd.ExcelWriter(fname, engine='xlsxwriter') as writer:
             df_c.to_excel(writer, sheet_name='cards', index=False)
             df_ac.to_excel(writer, sheet_name='accounts', index=False)
@@ -383,14 +324,6 @@ class KapitalAPI:
             t4.to_excel(writer, sheet_name='card-humo transactions', index=False)
             t5.to_excel(writer, sheet_name='accounts transactions', index=False)
 
-            # # get the workbook object to access its sheets
-            # workbook = writer.book
-            # # iterate over each sheet and freeze the first row
-            # for sheet in workbook.worksheets():
-            #     sheet.freeze_panes(1, 0)
-
             for sht_name in writer.sheets:
                 ws = writer.sheets[sht_name]
                 ws.freeze_panes(1,0)
-
-            # writer.save()
